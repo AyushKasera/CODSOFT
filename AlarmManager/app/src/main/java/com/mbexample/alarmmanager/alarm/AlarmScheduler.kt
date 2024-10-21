90a40d9b3477d3ab14eac4af91effa6214ebd531
Ayush Kasera <96968044+AyushKasera@users.noreply.github.com>
2024-10-21 23:31:15 +0530

    Initial commit

---
+++ AlarmManager/app/src/main/java/com/mbexample/alarmmanager/alarm/AlarmScheduler.kt
@@ -0,0 +1,10 @@
+package com.mbexample.alarmmanager.alarm
+
+import com.mbexample.alarmmanager.data.sources.local.Alarm
+
+interface AlarmScheduler {
+
+    fun schedule(alarm: Alarm)
+    //
+    fun cancel(alarm: Alarm)
+}
